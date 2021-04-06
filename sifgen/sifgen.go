c76f21226efffe34450c938f1d721384799aab2e
Charles Dusek <dusek@onomy.io>
2021-04-06 11:22:30 -0500

    Added docker deployment and sifgen from Sif Chain

---
+++ sifgen/sifgen.go
@@ -0,0 +1,82 @@
+package sifgen
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/Sifchain/sifnode/tools/sifgen/key"
+	"github.com/Sifchain/sifnode/tools/sifgen/network"
+	"github.com/Sifchain/sifnode/tools/sifgen/node"
+	"github.com/Sifchain/sifnode/tools/sifgen/utils"
+)
+
+type Sifgen struct {
+	chainID *string
+}
+
+func NewSifgen(chainID *string) Sifgen {
+	return Sifgen{
+		chainID: chainID,
+	}
+}
+
+func (s Sifgen) NewNetwork() *network.Network {
+	return &network.Network{
+		ChainID: *s.chainID,
+		CLI:     utils.NewCLI(*s.chainID),
+	}
+}
+
+func (s Sifgen) NetworkCreate(count int, outputDir, startingIPAddress string, outputFile string) {
+	net := network.NewNetwork(*s.chainID)
+	summary, err := net.Build(count, outputDir, startingIPAddress)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if err = ioutil.WriteFile(outputFile, []byte(*summary), 0600); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func (s Sifgen) NetworkReset(networkDir string) {
+	if err := network.Reset(*s.chainID, networkDir); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s Sifgen) NewNode() *node.Node {
+	return &node.Node{
+		ChainID: *s.chainID,
+		CLI:     utils.NewCLI(*s.chainID),
+	}
+}
+
+func (s Sifgen) NodeReset(nodeHomeDir *string) {
+	if err := node.Reset(*s.chainID, nodeHomeDir); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s Sifgen) KeyGenerateMnemonic(name, password *string) {
+	key := key.NewKey(name, password)
+	key.GenerateMnemonic()
+	fmt.Println(key.Mnemonic)
+}
+
+func (s Sifgen) KeyRecoverFromMnemonic(mnemonic string) {
+	key := key.NewKey(nil, nil)
+	if err := key.RecoverFromMnemonic(mnemonic); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(heredoc.Doc(`
+		Address: ` + key.Address + `
+		Validator Address: ` + key.ValidatorAddress + `
+		Consensus Address: ` + key.ConsensusAddress + `
+	`))
+}
